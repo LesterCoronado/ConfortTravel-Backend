49bde36baae88310679762fa63051d4185153c5d
LesterCoronado <lestercoronado730@gmail.com>
2024-10-17 09:37:42 -0600

    changes

--- Models/ConfortTravelContext.cs
+++ Models/ConfortTravelContext.cs
@@ -75,7 +75,7 @@ public partial class ConfortTravelContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=coronado; Database=ConfortTravel;  integrated security=True; Trusted_Connection=True;Encrypt=False;");
+        => optionsBuilder.UseSqlServer("server=conforttravel.database.windows.net; database=ConfortTravel; TrustServerCertificate=True; User Id=jbarrientos; Password=aj8<L)Ja1bp6;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
